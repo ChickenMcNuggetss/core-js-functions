129e1b8ed21c10d9c6af7faa377a1202c34474ff
KarinaI <7karina.ibr@gmail.com>
2024-04-11 23:44:00 +0300

    feat: add solution for memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,15 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return function cached(n) {
+    if (n in cache) {
+      return cache[n];
+    }
+    cache[n] = func(n);
+    return cache[n];
+  };
 }
 
 /**
