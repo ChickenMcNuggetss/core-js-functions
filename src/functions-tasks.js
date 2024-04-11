b981c7fcb912e5fd7914888c937f2a1b6c1f226e
KarinaI <7karina.ibr@gmail.com>
2024-04-12 00:54:40 +0300

    feat: add solution for logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -181,8 +181,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function runLog(...args) {
+    const argums = args.map((el) => JSON.stringify(el));
+    logFunc(`${func.name}(${argums}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${argums}) ends`);
+    return res;
+  };
 }
 
 /**
