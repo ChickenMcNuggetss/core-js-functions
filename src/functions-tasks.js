0462f97f54b760445acd8f50cd340f27c2dd66b0
KarinaI <7karina.ibr@gmail.com>
2024-04-11 23:24:10 +0300

    feat: add solution for getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -96,8 +96,11 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  const argums = args.reverse();
+  return function polynom(x) {
+    return argums.reduce((calc, el, i) => calc + el * x ** i, null);
+  };
 }
 
 /**
